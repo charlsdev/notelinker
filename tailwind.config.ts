d96f21db0f1e0d0b14469a0145bab4c4e5d7f447
charlsdev <youquince@gmail.com>
2023-11-12 22:16:08 -0500

    Add new module for ui components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
       './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
       './src/components/**/*.{js,ts,jsx,tsx,mdx}',
       './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+      "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
    ],
    theme: {
       extend: {
@@ -17,6 +18,6 @@ const config: Config = {
          },
       },
    },
-   plugins: [],
+   plugins: [nextui()],
 }
 export default config
