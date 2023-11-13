b0e92688f58bdb9dcf5997ec30e02069f3c24111
charlsdev <youquince@gmail.com>
2023-11-12 22:18:34 -0500

    Add modal for save data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import ModalAddUI from '@/components/AddModal'
+import { Button, useDisclosure } from '@nextui-org/react'
 // import { authOptions } from '../api/auth/[...nextauth]/route'
 // import { getServerSession } from 'next-auth/next'
 import { useSession } from 'next-auth/react'
@@ -11,22 +13,51 @@ export default function WelcomePage() {
    // const session = await getServerSession(authOptions as any)
    const { data: session, status } = useSession()
 
+   const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
    if (status === 'loading') {
       return <LoaderUI />
    }
 
    return (
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-800 text-slate-600 dark:text-white">
-         <div>
-            <h1 className="text-accent text-[5rem] font-extrabold leading-loose">
-               Hi {session?.user?.name}!
-            </h1>
+      <>
+         <div className="min-h-screen bg-gray-50 text-slate-600 dark:bg-gray-800 dark:text-white">
+            <div className="mt-1 border-b-2 border-zinc-800">
+               <div className="container mx-auto flex items-center justify-between px-4 py-4 md:px-2">
+                  <h1 className="text-2xl font-bold">Dashboard</h1>
+                  <Button
+                     size="sm"
+                     onPress={onOpen}
+                     color="warning"
+                     className="px-4 font-bold"
+                     startContent={
+                        <svg
+                           xmlns="http://www.w3.org/2000/svg"
+                           width="15"
+                           height="15"
+                           viewBox="0 0 24 24"
+                        >
+                           <g fill="none" stroke="currentColor" strokeWidth="1.5">
+                              <path d="m18.18 8.04l.463-.464a1.966 1.966 0 1 1 2.781 2.78l-.463.464M18.18 8.04s.058.984.927 1.853s1.854.927 1.854.927M18.18 8.04l-4.26 4.26c-.29.288-.434.433-.558.592c-.146.188-.271.39-.374.606c-.087.182-.151.375-.28.762l-.413 1.24l-.134.401m8.8-5.081l-4.26 4.26c-.29.29-.434.434-.593.558c-.188.146-.39.271-.606.374c-.182.087-.375.151-.762.28l-1.24.413l-.401.134m0 0l-.401.134a.53.53 0 0 1-.67-.67l.133-.402m.938.938l-.938-.938" />
+                              <path
+                                 strokeLinecap="round"
+                                 d="M8 13h2.5M8 9h6.5M8 17h1.5M3 14v-4c0-3.771 0-5.657 1.172-6.828C5.343 2 7.229 2 11 2h2c3.771 0 5.657 0 6.828 1.172M21 14c0 3.771 0 5.657-1.172 6.828m-15.656 0C5.343 22 7.229 22 11 22h2c3.771 0 5.657 0 6.828-1.172m0 0c.944-.943 1.127-2.348 1.163-4.828"
+                              />
+                           </g>
+                        </svg>
+                     }
+                  >
+                     Nuevo
+                  </Button>
+               </div>
+
+               <div className="container mx-auto px-4 md:px-2">
+
+               </div>
+            </div>
          </div>
-         <div>
-            <p>Protected client page</p>
-            <button className="btn btn-primary">Toggle</button>
-            <pre>{JSON.stringify(session, null, 2)}</pre>
-         </div>
-      </div>
+
+         <ModalAddUI isOpen={isOpen} onOpen={onOpen} onOpenChange={onOpenChange} />
+      </>
    )
 }
---
+++ src/components/AddModal.tsx
@@ -0,0 +1,116 @@
+import {
+   Modal,
+   ModalContent,
+   ModalHeader,
+   ModalBody,
+   ModalFooter,
+   Button,
+   Checkbox,
+   Input,
+   Link,
+} from '@nextui-org/react'
+
+interface ModalAddUIProps {
+   isOpen: boolean
+   onOpen: () => void
+   onOpenChange: () => void
+}
+
+export default function ModalAddUI({ isOpen, onOpen, onOpenChange }: ModalAddUIProps) {
+   return (
+      <>
+         <Modal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            placement="top-center"
+            backdrop='opaque'
+            isDismissable={false}
+         >
+            <ModalContent>
+               {onClose => (
+                  <>
+                     <ModalHeader className="flex flex-col gap-1">
+                        Log in
+                     </ModalHeader>
+                     <ModalBody>
+                        <Input
+                           autoFocus
+                           endContent={
+                              <svg
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 width="20"
+                                 height="20"
+                                 viewBox="0 0 24 24"
+                                 className="pointer-events-none flex-shrink-0 text-default-400"
+                              >
+                                 <g
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeWidth="1.5"
+                                 >
+                                    <path d="M22 14c0 3.771 0 5.657-1.172 6.828C19.657 22 17.771 22 14 22h-4c-3.771 0-5.657 0-6.828-1.172C2 19.657 2 17.771 2 14c0-3.771 0-5.657 1.172-6.828C3.825 6.518 4.7 6.229 6 6.102m12 0c1.3.127 2.175.416 2.828 1.07c.654.653.943 1.528 1.07 2.828M10 6h4m-3 3h2" />
+                                    <path d="M14 2.002c1.707.014 2.647.11 3.268.73c.732.732.732 1.91.732 4.267v2.064c0 .46 0 .69-.094.892c-.095.202-.272.35-.626.644l-.72.6M10 2.002c-1.707.014-2.647.11-3.268.73C6 3.464 6 4.642 6 6.999v2.064c0 .46 0 .69.094.892c.095.202.272.35.626.644l1.439 1.2c1.837 1.53 2.755 2.295 3.841 2.295c.65 0 1.239-.273 2-.82" />
+                                 </g>
+                              </svg>
+                           }
+                           label="Email"
+                           placeholder="Enter your email"
+                           variant="bordered"
+                        />
+                        <Input
+                           endContent={
+                              <svg
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 width="20"
+                                 height="20"
+                                 viewBox="0 0 24 24"
+                                 className="pointer-events-none flex-shrink-0 text-default-400"
+                              >
+                                 <g fill="none">
+                                    <path
+                                       fill="currentColor"
+                                       d="M9 16a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm4 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm4 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0Z"
+                                    />
+                                    <path
+                                       stroke="currentColor"
+                                       strokeLinecap="round"
+                                       strokeWidth="1.5"
+                                       d="M11 22H8c-2.828 0-4.243 0-5.121-.879C2 20.243 2 18.828 2 16c0-2.828 0-4.243.879-5.121C3.757 10 5.172 10 8 10h8c2.828 0 4.243 0 5.121.879C22 11.757 22 13.172 22 16c0 2.828 0 4.243-.879 5.121C20.243 22 18.828 22 16 22h-1M6 10V8c0-.34.028-.675.083-1m11.728-.5A6.003 6.003 0 0 0 7.528 4"
+                                    />
+                                 </g>
+                              </svg>
+                           }
+                           label="Password"
+                           placeholder="Enter your password"
+                           type="password"
+                           variant="bordered"
+                        />
+                        <div className="flex justify-between px-1 py-2">
+                           <Checkbox
+                              classNames={{
+                                 label: 'text-small',
+                              }}
+                           >
+                              Remember me
+                           </Checkbox>
+                           <Link color="primary" href="#" size="sm">
+                              Forgot password?
+                           </Link>
+                        </div>
+                     </ModalBody>
+                     <ModalFooter>
+                        <Button color="danger" variant="flat" onPress={onClose}>
+                           Close
+                        </Button>
+                        <Button color="primary" onPress={onClose}>
+                           Sign in
+                        </Button>
+                     </ModalFooter>
+                  </>
+               )}
+            </ModalContent>
+         </Modal>
+      </>
+   )
+}
